8affd40c2adc1fd5898f5386cb522ba9fd16bdc8
KimYongHwi <yhkim12389@gmail.com>
2022-12-31 01:40:47 +0900

    chore: delete files

--- src/main/java/com/delivery/kyh/adapter/out/persistence/DriverJpaRepository.java
+++
@@ -1,6 +0,0 @@
-package com.delivery.kyh.adapter.out.persistence;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DriverJpaRepository extends JpaRepository<DriverJpaEntity, Long> {
-}
